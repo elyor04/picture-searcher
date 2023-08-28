d678f3719e478aa95193c0c5245d9cfb374bfb3e
elyor04 <tuxtamuratovelyor@gmail.com>
2023-08-29 04:03:57 +0500

    just commit

--- AppMainWindow/appmainwindow.py
+++ AppMainWindow/appmainwindow.py
@@ -14,6 +14,18 @@ class AppMainWindow(QMainWindow, Ui_MainWindow):
 
     def _init(self) -> None:
         self.browseBtn.clicked.connect(self.browseBtn_clicked)
+        self.searchBtn.clicked.connect(self.searchBtn_clicked)
+        self.stopBtn.clicked.connect(self.stopBtn_clicked)
+        self.showBtn.clicked.connect(self.showBtn_clicked)
 
     def browseBtn_clicked(self) -> None:
+        pass
+
+    def searchBtn_clicked(self) -> None:
+        pass
+
+    def stopBtn_clicked(self) -> None:
+        pass
+
+    def showBtn_clicked(self) -> None:
         pass
