f539b9d653fa66dc7f54a14a6604bf6134e4236d
elyor04 <tuxtamuratovelyor@gmail.com>
2023-08-29 00:54:20 +0500

    just commit

--- AppMainWindow/appmainwindow.py
+++ AppMainWindow/appmainwindow.py
@@ -1,6 +1,11 @@
+"""
+pyuic6 -o AppMainWindow/ui_form.py "path/to/file.ui"
+"""
 from PyQt6.QtWidgets import QMainWindow, QWidget
+from .ui_form import Ui_MainWindow
 
 
-class AppMainWindow(QMainWindow):
+class AppMainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self, parent: QWidget = None) -> None:
         super().__init__(parent)
+        self.setupUi(self)
